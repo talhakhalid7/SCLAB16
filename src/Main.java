391454c3eed4ca003d9815ed4cfcfe08cae2b358
talhakhalid7 <talhakhalid7@gmail.com>
2016-05-30 17:59:26 +0500

    this is we done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+/*
+ * xkjsakjasbdkh
+ */
 		System.out.println("Enter first and second number:");
         Scanner inp= new Scanner(System.in);
         int num1,num2;
